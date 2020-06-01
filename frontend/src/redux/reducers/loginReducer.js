0dafc06c25f7839c680b226486bbdb9b1d826d58
dangxuanthang <dangxuanthangqt@gmail.com>
2020-06-01 21:08:52 +0700

    a

--- frontend/src/redux/reducers/loginReducer.js
+++ frontend/src/redux/reducers/loginReducer.js
@@ -1,16 +1,16 @@
 
 import produce from 'immer';
 import * as loginActionTypes from '../actionTypes/loginActionTypes';
-//import jwt_decode from 'jwt-decode';
-const initialState = "";
+import jwt_decode from 'jwt-decode';
+const initialState = {};
 var myReducer = (state = initialState, action) => (
     produce(state, draft => {
         switch (action.type) {
             case loginActionTypes.LOGIN_SUCCESS:
                 { let token = action.payload;
                  //   console.log(jwt_decode(token))
-                   // draft = jwt_decode(token);
-                   draft =token;
+                 draft = jwt_decode(token);
+                  // draft =token;
                 return draft;}
             case "LOGOUT_SUCCESS":
                 draft = {};
